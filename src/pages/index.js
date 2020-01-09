4782077d35e8185616f9d17b847610880b7d6946
屋比久博也 <yabiku@yabikuhiroyanoMacBook-Pro.local>
2020-01-09 16:34:39 +0900

    show article

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { graphql } from "gatsby"
+export default ({ data }) => {
+  return (
+    <div>
+      <h1>NEWS</h1>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <div>              
+            <span>{node.frontmatter.date}</span> : {node.frontmatter.title}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "YYYY年MM月DD日")
+          }
+        }
+      }
+    }
+  }
+`
